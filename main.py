d8ecb2fdf77ddc0ab76f737bebe638cb74f41745
Your Name <you@example.com>
2024-07-24 13:32:01 -0400

    Main - Ryan Klapmeyer

--- main.py
+++ main.py
@@ -1,38 +1,3 @@
-# Main - Ryan Klapmeyer
-
-def encode(passcode):
-    pass_list = list(passcode)
-    encode_list = []
-    for i in range(len(pass_list)):
-        if int(pass_list[i]) < 7:
-            pass_list[i] = int(pass_list[i]) + 3
-            encode_list.append(pass_list[i])
-        else:
-            pass_list[i] = (int(pass_list[i]) + 3) - 10
-            encode_list.append(pass_list[i])
-
-    encode_str = ''
-    for i in range(len(encode_list)):
-        encode_str += str(encode_list[i])
-
-    return encode_str
-
-def decode(passcode):
-    pass_list = list(passcode)
-    decode_list = []
-    for i in range(len(pass_list)):
-        if int(pass_list[i]) < 3:
-            pass_list[i] = int(pass_list[i]) + 7
-            decode_list.append(pass_list[i])
-        else:
-            pass_str = int(pass_list[i]) - 3
-            decode_list.append(pass_str)
-
-    decode_str = ''
-    for i in range(len(decode_list)):
-        decode_str += str(decode_list[i])
-
-    return decode_str
 
 def menu():
     print('Menu')
